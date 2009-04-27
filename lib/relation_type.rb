8739a75c166e8a2c6f849724b21085d01b7cfda6
tinogomes <tinorj@gmail.com>
2009-04-27 17:32:43 -0300

    Added dependent destroy

--- lib/relation_type.rb
+++ lib/relation_type.rb
@@ -1,3 +1,10 @@
 class RelationType < ActiveRecord::Base
   validates_presence_of :name
+  
+  after_destroy :destroy_all_friendsip_relations
+  
+  private
+    def destroy_all_friendsip_relations
+      FriendshipRelationType.delete_all :relation_id => self.id
+    end
 end
